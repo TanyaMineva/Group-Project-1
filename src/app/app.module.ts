1b34a024a43df1a2856da4e3d1c7d433ce48969e
NickD7 <nikola.kyulhanov@gmail.com>
2018-07-18 12:38:57 +0300

    Profile and Edit Profile forms configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,17 +12,21 @@ import { ErrorInterceptor } from './error-interceptor';
 import { ErrorComponent } from './error/error.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { PostModule } from './posts/posts.module';
-import { MyProfileComponent } from './profile/myprofile.component';
+import { EditMyProfileComponent } from './profile/edit-profile/edit-myprofile.component';
+import { ViewMyProfileComponent } from './profile/view-profile/view-myprofile.component';
 // import { PostService } from './posts/posts.service';
 
-@NgModule({         // We declare the classes
+@NgModule({
+  // We declare the classes
   declarations: [
     AppComponent,
     HeaderComponent,
     ErrorComponent,
-    MyProfileComponent
+    EditMyProfileComponent,
+    ViewMyProfileComponent
   ],
-  imports: [            // We import the components
+  imports: [
+    // We import the components
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
@@ -34,10 +38,10 @@ import { MyProfileComponent } from './profile/myprofile.component';
   ],
   // providers: [PostService],                   // providers are used for services
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},  // Dont overwrite interceptors, add an additional one
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, // Dont overwrite interceptors, add an additional one
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ],
   bootstrap: [AppComponent],
   entryComponents: [ErrorComponent]
 })
-export class AppModule { }
+export class AppModule {}
