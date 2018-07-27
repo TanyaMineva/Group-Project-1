aa164117abea5e18a5fb9b57966c7fc91e1821f0
TanyaMineva <37240382+TanyaMineva@users.noreply.github.com>
2018-07-27 11:34:31 +0300

    Search bar logic added. Profile filter available

--- src/app/posts/post-list/profile-list.component.ts
+++ src/app/posts/post-list/profile-list.component.ts
@@ -59,7 +59,21 @@ export class ProfileListComponent implements OnInit, OnDestroy {
     this.authStatusSub.unsubscribe();
   }
 
-
+  filterServices() {
+    let  input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
   
 }
 
